0217d118a87e2bbe6f2809ca56851b65391a7a31
KOALSOTO <koalsoto@yahoo.com>
2019-03-25 12:24:36 -0500

    use interface to reduce trie memory use

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// Indicates whether the trie rooted at this node contains the empty string.
+        /// </summary>
+        private bool _hasEmptyString = false;
+
+
+        /// <summary>
+        /// Adds the given string to the trie rooted at this node.
+        /// </summary>
+        /// <param name="s"> string to be added </param>
+        /// <returns></returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+            }
+            else
+            {
+                return new TrieWithOneChild(s, _hasEmptyString);
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Gets whether the trie rooted at this node contains the given string.
+        /// </summary>
+        /// <param name="s"> string to be looked up </param>
+        /// <returns></returns>
+        public bool Contains(string s)
+        {
+            return _hasEmptyString;
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// Indicates whether the trie rooted at this node contains the empty string.
+        /// </summary>
+        private bool _hasEmptyString;
+
+        /// <summary>
+        /// child of this node
+        /// </summary>
+        private ITrie _child;
+
+        /// <summary>
+        /// char of the child
+        /// </summary>
+        private char _childLabel;
+
+        /// <summary>
+        /// Constructs a trie containing the given string 
+        /// If s contains any characters other than lower-case English letters,
+        /// throws an ArgumentException.
+        /// </summary>
+        /// <param name="s"> The string to include. </param>
+        /// <param name="hasEmpty"></param>
+        public TrieWithOneChild(string s, bool hasEmpty)
+        {
+            if (s == "" || s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            _hasEmptyString = hasEmpty;
+            _childLabel = s[0];
+            _child = new TrieWithNoChildren().Add(s.Substring(1));
+        }
+
+        /// <summary>
+        /// Adds the given string to the trie rooted at this node.
+        /// </summary>
+        /// <param name="s"> string to be added </param>
+        /// <returns></returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else if (s[0] == _childLabel)
+            {
+                _child = _child.Add(s.Substring(1)); 
+            }
+            else
+            {
+                return new TrieWithManyChildren(s, _hasEmptyString, _childLabel, _child);
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Gets whether the trie rooted at this node contains the given string.
+        /// </summary>
+        /// <param name="s"> string to be looked up</param>
+        /// <returns></returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            if ( s[0] == _childLabel)
+            {
+                return _child.Contains(s.Substring(1));
+            }
+            return false;
+        }
+    }
+}
